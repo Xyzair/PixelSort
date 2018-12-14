e4077eccd60b7085037216a4794be885d3c97b79
Colby <xyzaire@gmail.com>
2018-12-13 23:27:19 -0800

    Added some menus

--- src/Gui.java
+++ src/Gui.java
@@ -1,12 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class Gui {
 
 	private static JFrame window;
+	private static JMenuBar menu;
+	private static ActionListener al;
+	
 	
 	public static void main(String args[])
 	{
 		setupWindow();
+		setupMenus();
 		window.setVisible(true);
 	}
 	
@@ -14,6 +21,34 @@ public class Gui {
 	{
 		window = new JFrame("Sorting Interface");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//window.setLayout(BorderLayout);
 		window.setSize(960, 540);
+		
+	}
+	
+	private static void setupMenus()
+	{
+		menu = new JMenuBar();
+		JMenu sorts = new JMenu("Sorts");
+		JMenu file = new JMenu("File");
+		
+		sorts.add(menuitem("test"));
+		
+		file.add(menuitem("Select Image"));
+		file.add(menuitem("Exit"));
+		
+		menu.add(file);
+		menu.add(sorts);
+		
+		
+		window.add(menu, BorderLayout.NORTH);
+	}
+	
+	private static JMenuItem menuitem(String name)
+	{
+		JMenuItem temp = new JMenuItem(name);
+		temp.addActionListener(al);
+		
+		return temp;
 	}
 }
