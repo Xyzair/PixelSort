cc74e7b80d29aadf2bf90c2f92c81c27ac57e822
Colby <xyzaire@gmail.com>
2018-12-17 15:11:19 -0800

    Got images to load in and show up in GUI
    need to work on getting both images to always show up

--- src/Gui.java
+++ src/Gui.java
@@ -2,8 +2,9 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import javax.swing.filechooser.*;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Gui implements ActionListener
@@ -11,43 +12,46 @@ public class Gui implements ActionListener
 
 	static JFrame window;
 	static JMenuBar menu;
-	static JMenuItem test;
 	static Gui al; //to be used as an ActionListener
 	static JFileChooser jfc;
+	JLabel image, sortedImage;
+	static File file;
 	
 	Gui()
 	{
 		super();
 		jfc = new JFileChooser();
+		
+		setupWindow();
+		setupMenus();
+		setupImagePanels();
 	}
 	
 	public static void main(String args[])
 	{
-		al = new Gui();
-		setupWindow();
-		setupMenus();
+		Gui gui = new Gui();
+
 		window.setVisible(true);
 	}
 	
-	private static void setupWindow()
+	private void setupWindow()
 	{
 		window = new JFrame("Sorting Interface");
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setLayout(new BorderLayout());
 		window.setSize(960, 540);
-		
 	}
 	
-	private static void setupMenus()
+	private void setupMenus()
 	{
 		menu = new JMenuBar();
 		JMenu sorts = new JMenu("Sorts");
 		JMenu file = new JMenu("File");
 		
-		test = menuitem("Test");
+		//sort menu tab
+		sorts.add(menuitem("Test"));
 		
-		sorts.add(test);
-		
+		//file menu tab
 		file.add(menuitem("Select Image"));
 		file.add(menuitem("Exit"));
 		
@@ -57,12 +61,43 @@ public class Gui implements ActionListener
 		window.add(menu, BorderLayout.NORTH);
 	}
 	
-	private static JMenuItem menuitem(String name)
+	private JMenuItem menuitem(String name)
 	{
 		JMenuItem temp = new JMenuItem(name);
-		temp.addActionListener(al);
+		temp.addActionListener(this);
 		
 		return temp;
+	}
+	
+	private void setupImagePanels()
+	{
+		//is there a point in this method?
+		image = loadImage("./src/TestingImages/ColorExplosion.jpg");
+		sortedImage = loadImage("./src/TestingImages/Scenic.png");
+		
+		window.add(image, BorderLayout.WEST);
+		window.add(sortedImage, BorderLayout.EAST);
+		
+	}
+	
+	private static JLabel loadImage(String path)
+	{
+		JLabel label;
+		
+		try 
+		{
+			//turns a file path -> buffered image -> imageIcon -> display in JLabel
+			label = new JLabel(new ImageIcon(ImageIO.read(new File(path))));
+			//JOptionPane.showMessageDialog(null, label);
+			return label;
+		} 
+		
+		catch (IOException e) 
+		{
+			
+		}
+		
+		return null;
 	}
 
 	public void actionPerformed(ActionEvent event)
@@ -80,19 +115,19 @@ public class Gui implements ActionListener
 			int returnVal = jfc.showOpenDialog(jfc);
 			
             if (returnVal == JFileChooser.APPROVE_OPTION) {
-            	File file = jfc.getSelectedFile();
+            	file = jfc.getSelectedFile();
             	//TODO set first image panel here
             }
 		}
 		
 		else if(contents.compareTo("Test") == 0)
 		{
-			System.out.println("Test successful1");
+			System.out.println("Test successful");
 		}
 		
 		else
 		{
-			System.out.println("Else1");
+			System.out.println("Else");
 		}
 	}
 }
