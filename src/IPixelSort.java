0f4f0d4d1c499f10abacbf3fae6168b56ad5c1de
Colby Biegler <xyzaire@gmail.com>
2020-04-27 02:23:52 -0700

    Add Javadoc comments to IPixelSort

--- src/IPixelSort.java
+++ src/IPixelSort.java
@@ -2,15 +2,35 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 
 public interface IPixelSort {
+
+    /**
+     *The sort method is where the sorting operations should be implemented.
+     */
     void sort();
 
+    /**
+     * Prints the image to a file and then returns the file location.
+     * @return The location of the sorted image file as a File
+     */
     File print();
 
+    /**
+     * @return original image as a BufferedImage
+     */
     BufferedImage getOriginalImage();
 
+    /**
+     * @return sorted image as a BufferedImage
+     */
     BufferedImage getSortedImage();
 
+    /**
+     * @return the file location of the sorted image as a File
+     */
     File getSortedFile();
 
+    /**
+     * @return some stats as a String about the image such as size
+     */
     String stats();
 }
